3617e1ed01d7fbaeed64633300c49b38556a8b8e
sandeep <pkytsky@gmail.com>
2021-10-19 15:04:31 +0530

    first commit

---
+++ docker-registry-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -o errexit
+
+# create registry container unless it already exists
+reg_name='docker-private-registry_docker-registry_1'
+reg_port='5001'
+running="$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)"
+if [ "${running}" != 'true' ]; then
+  docker-compose up --build -d
+  # docker run \
+  #   -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+  #   registry:2
+fi
+# connect the registry to the cluster network
+# (the network may already be connected)
+docker network connect "kind" "${reg_name}" || true
+
+# Document the local registry
+# https://github.com/kubernetes/enhancements/tree/master/keps/sig-cluster-lifecycle/generic/1755-communicating-a-local-registry
+
+kubectl apply -f registry-configmap.yaml
